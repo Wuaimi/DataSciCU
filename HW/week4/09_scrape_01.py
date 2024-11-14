b6320df647c0a56401d3ee7338cb2ad915cdf354
Wuaimi <10327sornpicha@gmail.com>
2024-11-14 13:06:32 +0700

    week4 homework

---
+++ HW/week4/09_scrape_01.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup
+from bs4.element import Tag
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def Q1(file_path): # DO NOT modify this line
+    _TH_FULL_WEEKDAYS = {
+    "วันจันทร์ที่" : 0,
+    "วันอังคารที่" : 1,
+    "วันพุธที่" : 2,
+    "วันพฤหัสบดีที่" : 3,
+    "วันศุกร์ที่": 4,
+    "วันเสาร์ที่": 5,
+    "วันอาทิตย์ที่" : 6,
+    }
+    day_list = [0,0,0,0,0,0,0]
+    with open(file_path) as f:
+        html = f.read()
+    soup = BeautifulSoup(html, "lxml")
+    all_bud_day = soup.select('div.bud-day')
+    for one_month in all_bud_day:
+        day = one_month.select('div.bud-day-col')[0].text.split(' ')[0]
+        day_list[_TH_FULL_WEEKDAYS[day]] +=1
+    return day_list
+
+
+def Q2(file_path): # DO NOT modify this line
+    with open(file_path) as f:
+        html = f.read()
+    soup = BeautifulSoup(html, "lxml")
+    imp_day = soup.select('div.bud-day-col a')
+    for day in imp_day:
+        if day.get("title")=="วันวิสาขบูชา":
+            vsk_day = day
+    for node in vsk_day.parent.previous_siblings:
+        if node.string.find("วัน")==0:
+            ans = node.string    
+    return ans
+
+exec(input().strip()) # do not delete this linea
--- mid_prepare/Mock_Midterm_Exam/student/student.py
+++ mid_prepare/Mock_Midterm_Exam/student/student.py
@@ -29,6 +29,12 @@ class exam:
            1. You must return the shape of the modified dataset to confirm changes were made correctly (return `self.data.shape`).
            2. Remember to update `self.data` as it will be used in subsequent methods.
         """
+        cols = ['survived','pclass', 'sex', 'age', 'sibsp', 'parch', 'fare', 'embarked', 'class', 'adult_male', 'alone']
+        self.df = self.df[cols]
+
+        
+        self.df.fillna(self.df.mean(numeric_only=True), inplace=True)
+        self.df.fillna(self.df.mode().iloc[0], inplace=True)
 
     def Q2(self): # DO NOT modify this line
         """
@@ -63,3 +69,38 @@ class exam:
         **Remarks:**
         1. Return the first element of the prediction array (`y_pred[0]`) to verify that the model outputs correctly.
         """
+
+
+import pandas as pd
+import numpy as np
+from student import *
+
+class Checker:
+    def __init__(self, exam):
+        # Assuming model is an instance of a class with methods Q1, Q2, Q3, and attributes like y_test
+        self.exam = exam
+
+    def Q1(self):
+        y = self.exam.Q1()
+        print(y)
+
+    def Q2(self):
+        self.exam.Q1()
+        y1 = self.exam.Q2()
+        print(f"{y1:.2f}")
+
+    def Q3(self):
+        self.exam.Q1()
+        self.exam.Q2()
+        y = self.exam.Q3()
+        print(y)
+
+def main():
+    data = pd.read_csv('/titanic.csv')
+    exam_titanic = exam(data)
+    checker = Checker(exam_titanic)
+    eval(f"checker.{input().strip()}()")
+
+
+if __name__ == "__main__":
+    main()
